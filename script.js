e0701cfc716a129701f2a9dd1c52276cd91dd9a2
GennyMcDonagh <gennymcdonagh@gmail.com>
2020-09-28 22:41:29 +1000

    move shaper size into function, wrap everything in iife, reset canvas at beginning of draw, make shape density dynamic based on canvas size

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+(function() {
 "use strict";
 const CANVAS_WIDTH = window.innerWidth;
 const CANVAS_HEIGHT = window.innerHeight;
@@ -8,18 +9,19 @@ canvas.height = CANVAS_HEIGHT;
 
 // variables for random positioning algorithm
 const numSamples = 50; 
-const points = []; 
-const numberOfShapes = 100;
+let points = []; 
+// adjust density based on screen size
+const numberOfShapes = canvas.width * canvas.height / 4000;
 
 const shapes = {
   rectangle: function (x,y) {
-    const width = getRandomIntInRange(10, 15);
-    const height = getRandomIntInRange(10, 15);
+    const width = randomSmallSize();
+    const height = randomSmallSize();
     ctx.fillRect(x, y, width, height);
   },
   
   circle: function (x,y) {
-    const diameter = getRandomIntInRange(10, 15);
+    const diameter = randomSmallSize();
     ctx.beginPath();
     ctx.arc(x, y, diameter/2, 0, Math.PI * 2, true);
     ctx.fill();
@@ -27,7 +29,7 @@ const shapes = {
   
   // x,y is the top corner of the triangle
   triangle: function (x,y) {
-    const width = getRandomIntInRange(10, 15);
+    const width = randomSmallSize();
     const height = width * Math.sqrt(3) / 2;
     ctx.beginPath();
     ctx.moveTo(x,y);
@@ -40,20 +42,25 @@ const shapes = {
 function init() {
   if (canvas.getContext) {
     ctx = canvas.getContext('2d');
+    canvas.addEventListener("click", drawShapes);
+    drawShapes();
   } else {
     console.log('canvas unsupported');
   }
-  canvas.addEventListener("click", drawMany);
 }
 
-function drawMany() {
+function drawShapes() {
+  //reset the canvas
+  points = [];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // todo clear interval
+
   let shapesDrawn = 0;
   const interval = setInterval(() => {
     ctx.fillStyle = randomColor();
     randomlyPosition(rotate((randomShape())));
     shapesDrawn++;
-
-    if (shapesDrawn === numberOfShapes) clearInterval(interval);
+    if (shapesDrawn >= numberOfShapes) clearInterval(interval);
   }, 5);
 }
 
@@ -61,6 +68,10 @@ function randomColor() {
   const maxColor = parseInt('ffffff', 16);
   const randomHex = getRandomIntInRange(0,maxColor);
   return `#${randomHex.toString(16)}`;
+}
+
+function randomSmallSize() {
+  return getRandomIntInRange(10, 15);
 }
 
 function randomShape() {
@@ -89,14 +100,13 @@ function randomlyPosition(shape) {
 }
 
 
-
 // Mitchell’s best-candidate algorithm
 // https://bost.ocks.org/mike/algorithms/
 // https://jsfiddle.net/pendensproditor/2XyV5
 function getRandomPosition() {
   let bestPoint, bestDistance = 0;
   for (let i = 0; i < numSamples; ++i) {
-    const newPoint = [Math.random() * CANVAS_WIDTH, Math.random() * CANVAS_HEIGHT];
+    const newPoint = [Math.random() * canvas.width, Math.random() * canvas.height];
     if (!points.length) {
       points.push(newPoint);
       return newPoint;
@@ -131,5 +141,6 @@ function findClosestDistance(newPoint) {
 }
 
 init();
+})();
 
 
